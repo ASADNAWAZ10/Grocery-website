f1bf73769f1fa5805200f63979f261752d0632dc
ASADNAWAZ10 <asadnawazasadnawaz211@gmail.com>
2026-04-13 09:29:52 -0600

    some changes

--- server/server.js
+++ server/server.js
@@ -3,20 +3,21 @@ import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import connectDB from './config/db.js';
 import 'dotenv/config';
-import userRouter from './routes/UserRoute.js';
+import UserRoute from './routes/UserRoute.js';
 import sellerRouter from './routes/SellerRoute.js';
-import connectCloudinary from './config/Cloudinary.js';
+import Cloudinary from './config/Cloudinary.js';
 import productRouter from './routes/productRoute.js'
-import cartRouter from './routes/CartRoute.js';
-import addressRouter from './routes/addressRoute.js';
-import orderRouter from './routes/orderRoute.js';
+import CartRoute from './routes/CartRoute.js';
+import addressRoute from './routes/addressRoute.js';
+import orderRoute from './routes/orderRoute.js';
+
 
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 await connectDB()
-await connectCloudinary()
+await Cloudinary()
 
 //Allow Multiple Origins
 const allowdOrigins = ["http://localhost:5173"]
@@ -27,12 +28,12 @@ app.use(cookieParser());
 app.use(cors({ origin: allowdOrigins, credentials: true }))
 
 app.get('/', async (req, res) => res.send('API is Working'))
-app.use('/api/user', userRouter)
+app.use('/api/user', UserRoute)
 app.use('/api/seller', sellerRouter)
 app.use('/api/product', productRouter)
-app.use('/api/cart', cartRouter)
-app.use('/api/address', addressRouter)
-app.use('/api/order', orderRouter)
+app.use('/api/cart', CartRoute)
+app.use('/api/address', addressRoute)
+app.use('/api/order', orderRoute)
 
 
 
